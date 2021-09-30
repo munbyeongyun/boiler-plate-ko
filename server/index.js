7b73d2b19f296f1e8536bc3b25d6359eb287b414
munbyeongyun <dkqk1326@naver.com>
2021-09-30 16:55:59 +0900

    프록시 설정까지

--- server/index.js
+++ server/index.js
@@ -1,6 +1,5 @@
 const express = require("express");
 const app = express();
-const port = 5000;
 const bodyParser = require("body-parser");
 const cookieParser = require("cookie-parser");
 const { auth } = require("./middleware/auth");
@@ -24,6 +23,8 @@ mongoose
 app.get("/", (req, res) => {
   res.send("Hello World! 안녕하세요");
 });
+
+app.get("/api/hello", (req, res) => res.send("안녕하세요 ~"));
 
 app.post("/api/users/register", (req, res) => {
   //회원 가입 할때 필요한 정보들을 client에서 가져오면
@@ -94,6 +95,8 @@ app.get("/api/users/logout", auth, (req, res) => {
     });
   });
 });
+
+const port = 5000;
 
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
