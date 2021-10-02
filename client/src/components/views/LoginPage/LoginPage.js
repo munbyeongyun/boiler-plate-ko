6d59a6fe811c34067eb69819b08e1fa7404787ff
munbyeongyun <dkqk1326@naver.com>
2021-10-02 16:52:33 +0900

    로그인 페이지

--- client/src/components/views/LoginPage/LoginPage.js
+++ client/src/components/views/LoginPage/LoginPage.js
@@ -1,7 +1,44 @@
-import React from "react";
+import React, { useState } from "react";
 
 function LoginPage() {
-  return <div>LoginPage</div>;
+  const [Email, setEmail] = useState("");
+  const [Password, setPassword] = useState("");
+
+  const onEmailHandler = (event) => {
+    setEmail(event.currentTarget.value);
+  };
+
+  const onPasswordHandler = (event) => {
+    setPassword(event.currentTarget.value);
+  };
+
+  const onSubmitHandler = (event) => {
+    event.preventDefult();
+  };
+
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "center",
+        alignItems: "center",
+        width: "100%",
+        height: "100vh",
+      }}
+    >
+      <from
+        style={{ display: "flex", flexDirction: "column" }}
+        onSubmit={onSubmitHandler}
+      >
+        <label>Email</label>
+        <input type="email" value={Email} onChange={onEmailHandler} />
+        <label>Password</label>
+        <input type="password" value={Password} onChange={onPasswordHandler} />
+        <br />
+        <button>Login</button>
+      </from>
+    </div>
+  );
 }
 
 export default LoginPage;
